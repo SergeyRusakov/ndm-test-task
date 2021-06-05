962c6e70d04b8a0db33bd9ba9bd9cf59dd827ce6
Sergey Rusakov <sergeyrusakov1@yandex.ru>
2021-06-05 12:18:24 +0500

    Add sidenav layout

---
+++ src/app/main/main.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SidenavComponent } from './components/sidenav/sidenav.component';
+import { MainComponent } from './components/main/main.component';
+import { RouterModule } from '@angular/router';
+
+@NgModule({
+  declarations: [
+    SidenavComponent,
+    MainComponent
+  ],
+  exports: [
+    MainComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterModule
+  ]
+})
+export class MainModule { }
